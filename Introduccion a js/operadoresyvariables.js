dd9391d60cef46887d43b27f15b094a5dd5fdddd
Demian <53890358+demian35@users.noreply.github.com>
2023-10-09 22:07:02 -0600

    uso de operadores aritmeticos en js

---
+++ Introduccion a js/operadoresyvariables.js
@@ -0,0 +1,34 @@
+//declaracion de variables
+let numero1=4;
+let numero2=2;
+let numero3=8
+
+let micadena="Helloworld";
+
+document.write(micadena);
+console.log(typeof micadena);//funcion typeof para ver el tipo de dato de una variable
+
+//uso de operadores matematicos
+
+//suma
+var suma=numero1+numero2;
+console.log(suma);
+console.log(typeof suma);
+
+//resta
+var resta=numero1-numero2;
+console.log(resta);
+console.log(typeof resta);
+
+//multiplicacion
+var mult=numero1*numero2;
+console.log(mult);
+console.log(typeof mult);
+
+//division
+var division=numero1/numero3;
+console.log(division);
+console.log(typeof division);
+
+
+
