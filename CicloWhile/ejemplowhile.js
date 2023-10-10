16445ffaab0fd4ec8663cd25eff69ffe015ea3f5
Demian <53890358+demian35@users.noreply.github.com>
2023-10-10 02:39:07 -0600

    funcion para sumar n numeros usando un for

--- CicloWhile/ejemplowhile.js
+++ CicloWhile/ejemplowhile.js
@@ -26,5 +26,19 @@ function sumaNumeros2(num){
     }while(i<=num);
     return suma;
 }
-
 console.log(sumaNumeros2(10));
+
+//funcion para sumar n numeros usando un for
+function sumaNumeros3(num){
+    suma=0;
+    for(let i=1;i<=num;i++){
+        suma+=i;
+    }
+    return suma;
+}
+
+console.log(sumaNumeros3(10));
+
+
+
+
