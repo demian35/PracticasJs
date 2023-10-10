e7fac4a3b1b4b911883f6dca899c1953747ba101
Demian <53890358+demian35@users.noreply.github.com>
2023-10-10 02:35:55 -0600

    creando funcion para sumar n numeros con do while

--- CicloWhile/ejemplowhile.js
+++ CicloWhile/ejemplowhile.js
@@ -1,9 +1,30 @@
+
+//funcion que suma los primeros n numeros del que reciba con un while
 function sumaNumeros(num){
     //inicializamos el contador;
-    let i=0;
+    let i=1;
+    let suma=0;//guardaremos cada ejecucion de suma de i en esta variable
     while(i<=num){
-        i+(i+1);
+        suma +=i;
+        i++
     }
+    return suma
 }
 
 console.log(sumaNumeros(10));
+
+
+//funcion que suma los primeros n numeros usando un do while
+
+function sumaNumeros2(num){
+    //inicializamos contador
+    let i=1;
+    suma=0;//iniciazamos la variable donde sumaremos las ejecuciones
+    do{
+        suma+=i;
+        i++
+    }while(i<=num);
+    return suma;
+}
+
+console.log(sumaNumeros2(10));
