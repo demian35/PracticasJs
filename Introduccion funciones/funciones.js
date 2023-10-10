59e053ad51c89c8a59c44b06600d3cce42709850
Demian <53890358+demian35@users.noreply.github.com>
2023-10-10 01:43:10 -0600

    creando funciones con operadores aritmeticos

--- Introduccion funciones/funciones.js
+++ Introduccion funciones/funciones.js
@@ -4,5 +4,18 @@ function saluda(nombre ="Pepe "){
     return `Hola ${nombre}`;
 }
 
+//funcion para sumar
+function suma(numero1,numero2){
+    return numero1+numero2;
+}
+
+//funcion para restar
+function resta(numero1,numero2){
+    return numero1-numero2;
+}
 
 console.log(saluda("gaelo"));
+
+console.log(suma(6,9));
+
+console.log(resta(15,8));
