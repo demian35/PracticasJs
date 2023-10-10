191aa060bbaa34a1e5459a79dcf46f526eb06292
Demian <53890358+demian35@users.noreply.github.com>
2023-10-10 02:07:27 -0600

    instanciando el objeto nomina para usar sus metodos

--- Introduccion funciones/ejemploPOO/Nomina.js
+++ Introduccion funciones/ejemploPOO/Nomina.js
@@ -1,6 +1,33 @@
 class Nomina{
+    
+    //contructor del objeto nomina
     constructor(nombre){
         this.nombre=nombre;
         this.empleados=[];
     }
+
+    //getter del nombre
+    getNombre(){
+        return this.nombre;
+    }
+
+    
+    //metodo para agregar un empleado en la nomina
+    agregaaNomina(empleado,sueldo){
+        this.empleados[empleado]=sueldo;
+    }
+
+    //metodo para consultar el sueldo de un empleado
+    consultaSueldo(empleado){
+        return this.empleados[empleado];
+    }
 }
+
+//creamos un objeto nomina
+
+let empleado= new Nomina("empleado");
+
+empleado.getNombre();
+
+empleado.agregaaNomina("fulano",10000);
+empleado.consultaSueldo("fulano");
