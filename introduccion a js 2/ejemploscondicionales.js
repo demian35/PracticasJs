bb2c28a9472d7e278cd12139d73e82c8a3ba7362
Demian <53890358+demian35@users.noreply.github.com>
2023-10-09 22:57:22 -0600

    ejemplo de condicional con switch

--- introduccion a js 2/ejemploscondicionales.js
+++ introduccion a js 2/ejemploscondicionales.js
@@ -7,3 +7,17 @@ if(suma >11 ){
 }else{
     document.write("el resultado es menor a 7");
 }
+
+//uso de estructura switch
+
+switch(suma){
+    case suma > 10:
+        console.log("El resultado es mayor a 10")
+        break;
+    case suma=10 || suma > 5:
+        console.log("El resultado esta entre 10 y 5")
+        break;
+    default:
+        console.log("el resultado es menor a 5")
+
+}
