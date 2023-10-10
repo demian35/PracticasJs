0d496546ca71097a3d3c4ed330c48ffc1f2e11cc
Demian <53890358+demian35@users.noreply.github.com>
2023-10-09 22:49:41 -0600

    ejemplo de uso de if condicional en js

---
+++ introduccion a js 2/ejemploscondicionales.js
@@ -0,0 +1,9 @@
+var suma=5+5
+
+if(suma >11 ){
+    document.write("LA suma es mayor que 11");
+}else if(suma<11 && suma >7){
+    document.write("El resultado esta entre 7 y 11");
+}else{
+    document.write("el resultado es menor a 7");
+}
