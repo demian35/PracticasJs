166e7d989f9f89af39233552380af49e1f05d0b9
Demian <53890358+demian35@users.noreply.github.com>
2023-10-11 02:19:50 -0600

    funcion con closures

--- usoClousure/ejemploClosure.js
+++ usoClousure/ejemploClosure.js
@@ -17,3 +17,40 @@ function global(){
 
 //ejecutando funcion con doble llamada
 console.log(global()());
+
+
+//ejemplo de funcion ejecutada con closure
+
+let miContador=(function (){
+    let _contador=0;//inicializamos el contador con la variable privada para que no sea accesible fuera de la funcion
+    function incremento(){//funcion que incrementa el contador
+        return _contador++;
+    }
+    function decremento(){//funcion que decrementa el contador
+        return _contador--;
+    }
+    function valorActual(){//regresamos el valor actual del contador
+        return _contador;
+    }
+
+    return{ //regresaremos la ejecucion de las tres funciones
+        incremento,
+        decremento,
+        valorActual,
+
+    }
+})();
+
+//ejecutamos las funciones
+//ejecucion valorActual
+console.log(miContador.valorActual());
+
+//ejecucion incremento
+console.log(miContador.incremento());
+console.log(miContador.valorActual());
+console.log(miContador.incremento());
+console.log(miContador.valorActual());
+
+//ejecucion decremento
+console.log(miContador.decremento());
+console.log(miContador.valorActual());
