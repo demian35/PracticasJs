81afbc6843556a7660a31aa45aa7d12687b1ce57
Demian <53890358+demian35@users.noreply.github.com>
2023-10-11 00:25:35 -0600

    ejemplo de sort,pop,push aplicado a un array

--- ejemplosArreglos/creacionArreglos.js
+++ ejemplosArreglos/creacionArreglos.js
@@ -1,2 +1,20 @@
 let miArreglo=[[2,3],[5,7]];
 console.log(miArreglo[1][1])
+
+//metodos de arrays
+
+let miArreglo2=[2,5,7,3,8];
+
+//sort para ordenar los elementos del arreglo
+console.log(miArreglo2.sort());
+
+//elimina el ultmo elemento del arreglo
+console.log(miArreglo2.pop());
+
+//agrega elementos al array
+console.log(miArreglo2.push(9));
+
+console.log(miArreglo2.sort());
+
+
+
