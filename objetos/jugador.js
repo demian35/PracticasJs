8f0b8c693f102700b6f3c865a326b65eb8b9ed17
Demian <53890358+demian35@users.noreply.github.com>
2023-10-10 21:26:17 -0600

    comparacion de objetos y uso de funciones math

--- objetos/jugador.js
+++ objetos/jugador.js
@@ -19,3 +19,46 @@ console.log(jugador["club"]);
 
 console.log(jugador.juega());
 
+
+//creando otro objeto
+
+const libro={
+    titulo:"Programacion1",
+    autor:{//Creando una estructura dentro de otra esructura
+        nombre:"Fulanito",
+        edad:30,
+        redes:{
+            tiktok:"fulanitomx"
+        }
+    }
+};
+
+console.log(libro.autor.redes.tiktok);//accediendo al atributo tiktok
+
+const coche1={marca:"tesla",modelo:"X"}
+const coche2={marca:"tesla",modelo:"X"}
+
+//comparando objetos
+
+if(coche1===coche2){
+    console.log("Son coches iguales")
+}else{
+    console.log("Son coches diferentes")
+}
+
+
+//comparando objetos por sus atributos
+if(coche1.marca==coche2.marca){
+    console.log("Son coches iguales");
+}else{
+    console.log("Son coches diferentes");
+}
+
+
+//usos de funciones de Math
+
+console.log(Math.PI);//imprimimos el valor de pi
+console.log(Math.random());//imprimimos un numero aleatorio entre 0 y 1
+console.log(Math.min(2,4,7));//busca el numero mas chico
+console.log(Math.max(2,6,8));//busca el numero mas grande
+
