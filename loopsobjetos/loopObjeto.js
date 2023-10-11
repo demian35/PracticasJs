69ac2d54b23aec98a31e45bccfb920235166c799
Demian <53890358+demian35@users.noreply.github.com>
2023-10-10 21:05:30 -0600

    mostramos los atributos del array

--- loopsobjetos/loopObjeto.js
+++ loopsobjetos/loopObjeto.js
@@ -5,3 +5,9 @@ const jugador={
     edad:25,
     posicion:StylePropertyMapReadOnly
 };
+
+const propiedades=Object.getOwnPropertyNames(jugador);//obtenemos las propiedades del objeto
+propiedades.forEach(name =>{
+    let valor=Object.getOwnPropertyDescriptor(jugador,name).value;//nos regresa la informacion de un objeto
+    console.log(`La propiedad ${name} Tiene: ${valor}`)
+})
